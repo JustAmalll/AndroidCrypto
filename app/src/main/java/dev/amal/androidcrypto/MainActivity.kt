a44aa49311a22a5e180fc1f3ae6ae3b6b230a144
JustAmalll <dev.justamal@gmail.com>
2023-03-24 15:00:24 +0600

    implemented: Encryption & Decryption

--- app/src/main/java/dev/amal/androidcrypto/MainActivity.kt
+++ app/src/main/java/dev/amal/androidcrypto/MainActivity.kt
@@ -3,14 +3,79 @@ package dev.amal.androidcrypto
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import dev.amal.androidcrypto.ui.theme.AndroidCryptoTheme
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val cryptoManager = CryptoManager()
+
         setContent {
             AndroidCryptoTheme {
+                var messageToEncrypt by remember { mutableStateOf("") }
+                var messageToDecrypt by remember { mutableStateOf("") }
 
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colors.background)
+                        .padding(32.dp)
+                ) {
+                    TextField(
+                        value = messageToEncrypt,
+                        onValueChange = { messageToEncrypt = it },
+                        modifier = Modifier.fillMaxWidth(),
+                        placeholder = { Text(text = "Encrypt string") }
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Row {
+                        Button(
+                            onClick = {
+                                val bytes = messageToEncrypt.encodeToByteArray()
+                                val file = File(filesDir, "secret.txt")
+                                if (!file.exists()) file.createNewFile()
+                                val fos = FileOutputStream(file)
+
+                                messageToDecrypt = cryptoManager.encrypt(
+                                    bytes = bytes, outputStream = fos
+                                ).decodeToString()
+                            }
+                        ) {
+                            Text(text = "Encrypt")
+                        }
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(
+                            onClick = {
+                                val file = File(filesDir, "secret.txt")
+                                messageToEncrypt = cryptoManager.decrypt(
+                                    inputStream = FileInputStream(file)
+                                ).decodeToString()
+                            }
+                        ) {
+                            Text(text = "Decrypt")
+                        }
+                    }
+                    Text(
+                        text = messageToDecrypt,
+                        color = MaterialTheme.colors.onBackground
+                    )
+                }
             }
         }
     }
